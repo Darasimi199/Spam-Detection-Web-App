8216b0e3c10cd6c66945fcd5b747f92663060c76
darasimi199 <daraoyebisi@gmail.com>
2023-02-14 07:01:03 +0100

    web app

---
+++ spam/Spam_2/st_app.py
@@ -0,0 +1,33 @@
+import pickle
+import streamlit as st
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+
+model = pickle.load(open(r"C:\Users\Darasimi199\Documents\ML_Projects\spam\SVM_model.pkl", 'rb'))
+
+def main():
+    st.title('Message Detection Solution')
+
+    #load in the dataset
+    df = pd.read_csv(r'C:\Users\Darasimi199\Documents\ML_Projects\spam\clean_df.csv')
+
+    # Input variable 'message'
+    message = st.text_input('Message')
+    message = [message]
+
+    # Fit transform text into vectorizer
+    tf_vec = TfidfVectorizer().fit(df['messages'])
+    features = tf_vec.transform(message)
+    features = features.toarray()
+
+    # Prediction code
+    if st.button('Predict'):
+        make_pred = model.predict(features)
+        if make_pred[0]==0:
+            st.success('This message is Ham')
+        else:
+            st.success('This message is Spam')
+
+if __name__ == '__main__':
+    main()
